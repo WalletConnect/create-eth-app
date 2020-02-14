5f35bc0a331df8834183be9db354de977cc57bb3
Paul Razvan Berg <hello@paulrberg.com>
2020-02-14 19:37:20 +0200

    fix: make shouldUseYarnWorkspaces work on Windows

--- src/helpers/yarn.ts
+++ src/helpers/yarn.ts
@@ -2,20 +2,24 @@ import chalk from "chalk";
 import os from "os";
 import { execSync } from "child_process";
 
-export function shouldUseYarn() {
+export function shouldUseYarn(): void {
   try {
     execSync("yarnpkg --version", { stdio: "ignore" });
-    return true;
   } catch (e) {
-    return false;
+    console.error(
+      "Yarn is necessary for Create Eth App. Install it by following the official documentation:",
+    );
+    console.log();
+    console.log(chalk.cyan("  https://classic.yarnpkg.com/en/docs/install"));
+    process.exit(1);
   }
 }
 
-export function shouldUseYarnWorkspaces() {
-  const isWorkspacesActivated: string = execSync("yarn config get workspaces-experimental", { encoding: "utf8" });
-  if (isWorkspacesActivated.replace(os.EOL, "") !== "true") {
+export function shouldUseYarnWorkspaces(): void {
+  const result: string = execSync("yarnpkg config get workspaces-experimental", { encoding: "utf8" });
+  if (result.replace(os.EOL, "") !== "true") {
     console.error(
-      `The workspaces feature is necessary for Create Eth App but is disabled in your Yarn config. Please activate it by running this command:`,
+      "The Yarn Workspaces feature is necessary for Create Eth App. Please enable it by running this command:",
     );
     console.log();
     console.log(chalk.cyan("  yarn config set workspaces-experimental true"));
