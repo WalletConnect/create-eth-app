e251599c94b8c5c6cd5759ffc68cd696279ad584
Paul Razvan Berg <hello@paulrberg.com>
2020-05-09 14:24:26 +0300

    refactor: move default template in this repo

---
+++ templates/react/default/packages/contracts/src/index.js
@@ -0,0 +1,2 @@
+export { default as abis } from "./abis.js";
+export { default as addresses } from "./addresses.js";
---
+++ templates/react/default/packages/react-app/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import "./index.css";
+import App from "./App";
+
+// You should replace this uri with your own and put it into a .env file
+// See all subgraphs: https://thegraph.com/explorer/
+const client = new ApolloClient({
+  uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root"),
+);
