6f1b7810523c8b6ee3177ad2e2c3b41aecb16139
Paul Razvan Berg <hello@paulrberg.com>
2020-07-09 15:34:43 +0300

    feat: kyber template

---
+++ templates/vue/kyber/packages/contracts/src/abis.js
@@ -0,0 +1,25 @@
+import conversionRates from "./abis/conversionRates.json";
+import erc20 from "./abis/erc20.json";
+import kyberFeeHandler from "./abis/kyberFeeHandler.json";
+import kyberHintHandler from "./abis/kyberHintHandler.json";
+import kyberNetworkProxyV1 from "./abis/kyberNetworkProxyV1.json";
+import kyberNetworkProxyV2 from "./abis/kyberNetworkProxyV2.json";
+import kyberReserve from "./abis/kyberReserve.json";
+import kyberStorage from "./abis/kyberStorage.json";
+import liquidityConversionRates from "./abis/liquidityConversionRates.json";
+import sanityRates from "./abis/sanityRates.json";
+import simpleKyberProxy from "./abis/simpleKyberProxy.json";
+
+export default {
+  conversionRates,
+  erc20,
+  kyberFeeHandler,
+  kyberHintHandler,
+  kyberNetworkProxyV1,
+  kyberNetworkProxyV2,
+  kyberReserve,
+  kyberStorage,
+  liquidityConversionRates,
+  sanityRates,
+  simpleKyberProxy,
+};
