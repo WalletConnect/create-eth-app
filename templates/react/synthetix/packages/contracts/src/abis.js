98d2a2a2f3d16e66a068f7d4cac4999598d12bf0
Paul Razvan Berg <hello@paulrberg.com>
2020-07-17 18:19:15 +0300

    refactor: prune most contracts from the synthetix template
    refactor: fully templatify the readOnChainData function

--- templates/react/synthetix/packages/contracts/src/abis.js
+++ templates/react/synthetix/packages/contracts/src/abis.js
@@ -1,55 +1,9 @@
 import SNX from "./abis/snx.json";
 import addressResolver from "./abis/addressResolver.json";
-import delegateApprovals from "./abis/delegateApprovals.json";
-import depot from "./abis/depot.json";
-import escrowChecker from "./abis/escrowChecker.json";
-import eternalStorage from "./abis/eternalStorage.json";
-import exchanger from "./abis/exchanger.json";
-import exchangeRates from "./abis/exchangeRates.json";
-import exchangeState from "./abis/exchangeState.json";
-import feePool from "./abis/feePool.json";
-import feePoolEternalStorage from "./abis/feePoolEternalStorage.json";
-import feePoolState from "./abis/feePoolState.json";
-import issuanceEternalStorage from "./abis/issuanceEternalStorage.json";
-import issuer from "./abis/issuer.json";
-import liquidations from "./abis/liquidations.json";
-import proxy from "./abis/proxy.json";
-import proxyErc20 from "./abis/proxyErc20.json";
-import purgeableSynth from "./abis/purgeableSynth.json";
-import rewardEscrow from "./abis/rewardEscrow.json";
-import rewardsDistribution from "./abis/rewardsDistribution.json";
-import supplySchedule from "./abis/supplySchedule.json";
-import synth from "./abis/synth.json";
-import synthetixEscrow from "./abis/synthetixEscrow.json";
-import snxState from "./abis/snxState.json";
-import systemStatus from "./abis/systemStatus.json";
-import tokenState from "./abis/tokenState.json";
 
 export default {
-  SNX,
   addressResolver,
-  delegateApprovals,
-  depot,
-  escrowChecker,
-  eternalStorage,
-  exchanger,
-  exchangeRates,
-  exchangeState,
-  feePool,
-  feePoolEternalStorage,
-  feePoolState,
-  issuanceEternalStorage,
-  issuer,
-  liquidations,
-  proxy,
-  proxyErc20,
-  purgeableSynth,
-  rewardEscrow,
-  rewardsDistribution,
-  supplySchedule,
-  synth,
-  synthetixEscrow,
-  snxState,
-  systemStatus,
-  tokenState
+  tokens: {
+    SNX,
+  },
 };
