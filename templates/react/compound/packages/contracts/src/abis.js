d5b929a9766756b274151be7dcb2fcc69aacd663
Paul Razvan Berg <hello@paulrberg.com>
2020-07-30 23:53:02 +0300

    feat: update compound template to latest protocol version
    fixes #39

--- templates/react/compound/packages/contracts/src/abis.js
+++ templates/react/compound/packages/contracts/src/abis.js
@@ -1,30 +1,20 @@
 import COMP from "./abis/COMP.json";
-import base0bps_Slope2000bps from "./abis/base0bps_Slope2000bps.json";
-import base200bps_Slope222bps_Kink90_Jump10 from "./abis/base200bps_Slope222bps_Kink90_Jump10.json";
-import base200bps_Slope3000bps from "./abis/base200bps_Slope3000bps.json";
-import base500bps_Slope1200bps from "./abis/base500bps_Slope1200bps.json";
 import cBAT from "./abis/cBAT.json";
 import cDAI from "./abis/cDAI.json";
 import cETH from "./abis/cETH.json";
 import cREP from "./abis/cREP.json";
 import cSAI from "./abis/cSAI.json";
-import cTBTC from "./abis/cTBTC.json";
 import cUSDC from "./abis/cUSDC.json";
+import cUSDT from "./abis/cUSDT.json";
 import cWBTC from "./abis/cWBTC.json";
 import cZRX from "./abis/cZRX.json";
 import comptroller from "./abis/comptroller.json";
-import daiRateModel from "./abis/daiRateModel.json";
 import governance from "./abis/governance.json";
 import priceOracle from "./abis/priceOracle.json";
 import timelock from "./abis/timelock.json";
 
 export default {
-  base0bps_Slope2000bps,
-  base200bps_Slope222bps_Kink90_Jump10,
-  base200bps_Slope3000bps,
-  base500bps_Slope1200bps,
   comptroller,
-  daiRateModel,
   governance,
   priceOracle,
   timelock,
@@ -35,8 +25,8 @@ export default {
     cETH,
     cREP,
     cSAI,
-    cTBTC,
     cUSDC,
+    cUSDT,
     cWBTC,
     cZRX,
   },
