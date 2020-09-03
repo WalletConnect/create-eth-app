302dce3f4c60473b300af6aa03b370f3e8a82668
Tom French <tom@tomfren.ch>
2020-09-03 01:33:35 +0100

    fix: give an invalid infura key to get around WalletConnect's test

--- handlebars/react/packages/react-app/src/utils/web3Modal.js
+++ handlebars/react/packages/react-app/src/utils/web3Modal.js
@@ -1,9 +1,9 @@
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
-// Please replace this with your own Infura ID
-// You can get a free key at https://infura.io/register
-const INFURA_ID = "8e1441aac766490a8671d8800c334c64";
+// Enter a valid infura key here to avoid being rate limited
+// You can get a key for free at https://infura.io/register
+const INFURA_ID = "INVALID_INFURA_KEY";
 
 // Web3Modal also supports many other wallets.
 // You can see other options at https://github.com/Web3Modal/web3modal
