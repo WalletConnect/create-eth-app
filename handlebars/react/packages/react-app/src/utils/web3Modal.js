eb21b651511e541fb91abf7acf3e686825f7978d
Tom French <tom@tomfren.ch>
2020-09-03 01:16:05 +0100

    feat: add comments for INFURA_ID and web3Modal docs

--- handlebars/react/packages/react-app/src/utils/web3Modal.js
+++ handlebars/react/packages/react-app/src/utils/web3Modal.js
@@ -1,8 +1,12 @@
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
+// Please replace this with your own Infura ID
+// You can get a free key at https://infura.io/register
 const INFURA_ID = "8e1441aac766490a8671d8800c334c64";
 
+// Web3Modal also supports many other wallets.
+// You can see other options at https://github.com/Web3Modal/web3modal
 export const web3Modal = new Web3Modal({
   network: "mainnet",
   cacheProvider: true,
