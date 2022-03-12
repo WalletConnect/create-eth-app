4d2903daa9aec0d5d199cf7ca0f6885a55893fda
Paul Razvan Berg <hello@paulrberg.com>
2022-03-12 16:25:28 +0000

    build: bump dependency versions in react template
    refactor: update to latest subgraph "specVersion" and "apiVersion"
    refactor: rewrite subgraph mappings to be compatible with latest API

--- templates/vue/default/packages/subgraph/src/mappings/transfers.ts
+++ templates/vue/default/packages/subgraph/src/mappings/transfers.ts
@@ -1,13 +1,19 @@
+import { Address } from "@graphprotocol/graph-ts";
+
 import { Transfer as TransferEvent } from "../types/CeaErc20/erc20";
 import { Transfer } from "../types/schema";
 import { addToken } from "./tokens";
 
 export function handleTransfer(event: TransferEvent): void {
-  let transactionHash = event.transaction.hash.toHex();
+  let transactionHash: string = event.transaction.hash.toHex();
   let transfer = new Transfer(transactionHash);
   transfer.from = event.params.from.toHex();
   transfer.to = event.params.to.toHex();
   transfer.value = event.params.value;
   transfer.save();
-  addToken(event.transaction.to.toHex());
+
+  let to: Address | null = event.transaction.to;
+  if (to) {
+    addToken(to.toHex());
+  }
 }
