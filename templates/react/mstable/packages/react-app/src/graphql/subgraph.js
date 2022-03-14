b3aea43bb58cfd506f384f41ef5eff4693b4e0a2
Paul Razvan Berg <hello@paulrberg.com>
2022-03-14 10:02:34 +0000

    refactor: switch to simpler "@apollo/client" configuration
    build: add "@apollo/client" dep
    build: remove "apollo-boost", "@apollo-client", "apollo-utilities", "graphql" deps

--- templates/react/mstable/packages/react-app/src/graphql/subgraph.js
+++ templates/react/mstable/packages/react-app/src/graphql/subgraph.js
@@ -1,4 +1,4 @@
-import { gql } from "apollo-boost";
+import { gql } from "@apollo/client";
 
 // See more example queries on https://thegraph.com/explorer/subgraph/mstable/mstable-protocol
 const GET_MASSETS = gql`
