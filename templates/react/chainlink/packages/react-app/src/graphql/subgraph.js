6113de9dc9efb1309fd87eae5abf202097c6f481
Paul Razvan Berg <hello@paulrberg.com>
2022-03-15 12:38:56 +0000

    fix: typos in chainlink template

--- templates/react/chainlink/packages/react-app/src/graphql/subgraph.js
+++ templates/react/chainlink/packages/react-app/src/graphql/subgraph.js
@@ -1,4 +1,4 @@
-import { gql } from "apollo-boost";
+import { gql } from "@apollo/client";
 
 // See more example queries on https://thegraph.com/legacy-explorer/subgraph/tomafrench/chainlink
 const GET_PRICES_FEEDS = gql`
