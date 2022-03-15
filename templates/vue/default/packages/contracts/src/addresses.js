b052da77b2afb55bae439fc79edb4f93de3d5759
Paul Razvan Berg <hello@paulrberg.com>
2022-03-15 09:39:45 +0000

    refactor: switch to using newer CeaErc20 contract in default templates

--- templates/vue/default/packages/contracts/src/addresses.js
+++ templates/vue/default/packages/contracts/src/addresses.js
@@ -1,7 +1,7 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
+  ceaErc20: "0xa6dF0C88916f3e2831A329CE46566dDfBe9E74b7",
 };
 
 export default addresses;
--- templates/vue/default/packages/subgraph/src/mappings/tokens.ts
+++ templates/vue/default/packages/subgraph/src/mappings/tokens.ts
@@ -7,7 +7,7 @@ export function addToken(address: string): void {
   }
 
   token = new Token(address);
-  if (address == "0xc1c0472c0c80bccdc7f5d01a376bd97a734b8815") {
+  if (address == "0xa6dF0C88916f3e2831A329CE46566dDfBe9E74b7") {
     token.decimals = 18;
     token.name = "CeaErc20";
     token.symbol = "CEAERC20";
